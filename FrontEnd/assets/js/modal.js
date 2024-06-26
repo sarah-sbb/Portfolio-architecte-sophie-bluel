ee56b3cdf04564fef96096095b989892666ab800
Sarah BASSET <boha.design@outlook.fr>
2024-06-26 15:24:45 +0200

    feature(add photo) : add view "add photo" at the modal in javascript

---
+++ FrontEnd/assets/js/modal.js
@@ -0,0 +1,129 @@
+//******* MODAL *******//
+let modal = null; //permet de savoir quelle boite modal est ouverte
+
+// Fonction pour ouvrir la modale
+const openModal = (e) => {
+    e.preventDefault();
+    const target = document.getElementById('modal1');
+    target.style.display = null; //retire le display none
+    target.removeAttribute('aria-hidden');
+    target.setAttribute('aria-modal', 'true');
+    modal = target; //permet de savoir quelle boite modal est ouverte
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
+    setTitle('Galerie photo');
+}
+
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal);
+});
+
+// Fonction pour fermer la modale
+const closeModal = (e) => {
+    if (modal === null) return;
+    e.preventDefault();
+    modal.style.display = "none";
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
+    modal = null;
+}
+
+const stopPropagation = (e) => {
+    e.stopPropagation();
+}
+
+document.querySelectorAll('.js-modal').forEach(a => {
+    a.addEventListener('click', openModal);
+})
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape" || e.key === "Esc") {
+        closeModal(e);
+    }
+})
+
+// Fonction pour afficher la galerie modale
+function displayModalGallery(data) {
+    const modalGallery = document.querySelector(".modalGallery");
+    modalGallery.innerHTML = "";
+    data.forEach((project) => {
+        createModalProjectCard(project);
+    });
+}
+
+// Fonction pour créer une carte de projet dans la galerie modale
+function createModalProjectCard(project) {
+    const modalGallery = document.querySelector(".modalGallery");
+    const workGalleryCard = document.createElement("figure");
+    const workImage = document.createElement("img");
+    const trashCan = document.createElement("i");
+    trashCan.classList.add("fa-solid", "fa-trash-can");
+
+    workImage.src = project.imageUrl;
+    workImage.alt = project.title;
+    workGalleryCard.dataset.category = project.category.name;
+    workGalleryCard.dataset.id = project.id;
+    workGalleryCard.className = "workGalleryCard";
+    workGalleryCard.append(workImage, trashCan);
+    modalGallery.appendChild(workGalleryCard);
+
+    // Ajout de l'écouteur d'événement pour la suppression
+    trashCan.addEventListener("click", function () {
+        deleteProject(project.id, workGalleryCard);
+    });
+}
+
+// Fonction pour supprimer un projet
+function deleteProject(workId, workGalleryCard) {
+    const authToken = localStorage.getItem('authToken');
+
+    fetch(`${catchAPIurl}/works/${workId}`, {
+        method: "DELETE",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${authToken}`
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            workGalleryCard.remove();
+            console.log(`Votre projet ${workId} a été supprimé avec succès`);
+        } else {
+            console.error(`Erreur lors de la suppression`);
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+// Fonction pour définir le titre de la modal
+function setTitle(title) {
+    const titleElement = document.getElementById('titlemodal');
+    titleElement.textContent = title;
+}
+
+//******* ADD PHOTO *******//
+// fonction pour ajouter une photo
+const addPhotoButton = document.getElementById('addPhoto');
+addPhotoButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    const modalGallery = document.querySelector('.modalGallery');
+    const modalForm = document.getElementById('modalForm');
+    
+    // Masquer le modalWrapper et afficher le modalForm
+    modalGallery.style.display = 'none';
+    modalForm.style.display = 'block';
+    
+    modalForm.removeAttribute('aria-hidden');
+    modalForm.setAttribute('aria-modal', 'true');
+
+    // Masquer le bouton "Ajouter une photo"
+    addPhotoButton.style.display = 'none';
+
+    // Changer le titre de la modal
+    setTitle('Ajout photo');
+});
--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,63 +1,63 @@
 const catchAPIurl = "http://localhost:5678/api";
 
-// Appel de l'API avec fetch
+// Fonction pour charger les travaux lors du chargement de la page
 window.onload = () => {
-    fetch(`${catchAPIurl}/works`)
-      .then((response) => response.json())
-      .then((data) => {
-        //get list of categories
-        const categories = getCategories(data);
-        //affiche tous les travaux
-        displayGallery(data);
-        //Filter functionnality
-        const filter = document.querySelector(".filter");
-        categoryFilter(categories, filter);
-        //administrator mode
-        // adminUserMode(filter);
-        displayModalGallery(data);
-      });
+    fetchWorks();
 };
 
-//*******GALLERY*******//
+// Fonction pour récupérer et afficher les travaux
+function fetchWorks() {
+    fetch(`${catchAPIurl}/works`)
+        .then(response => response.json())
+        .then(data => {
+            // récupérer les div des catégories et des works pour les vider en faisant un inner HTML
+            const categories = getCategories(data);
+            displayGallery(data);
+            const filter = document.querySelector(".filter");
+            categoryFilter(categories, filter);
+            displayModalGallery(data);
+        });
+}
 
+//******* GALLERY *******//
+
+// Fonction pour afficher la galerie principale
 function displayGallery(data) {
     const gallery = document.querySelector(".gallery");
     gallery.innerHTML = "";
-    //intégre tous les travaux dans un tableau
-    data.forEach((i) => {
-      //crée les tags
-      const workCard = document.createElement("figure");
-      const workImage = document.createElement("img");
-      const workTitle = document.createElement("figcaption");
-      workImage.src = i.imageUrl;
-      workImage.alt = i.title;
-      workTitle.innerText = i.title;
-      workCard.dataset.category = i.category.name;
-      workCard.className = "workCard";
-      //references to DOM
-      gallery.appendChild(workCard);
-      workCard.append(workImage, workTitle);
+    data.forEach((project) => {
+        createProjectCard(project);
     });
+}
+
+// Fonction pour créer une carte de projet dans la galerie principale
+function createProjectCard(project) {
+    const gallery = document.querySelector(".gallery");
+    const workCard = document.createElement("figure");
+    const workImage = document.createElement("img");
+    const workTitle = document.createElement("figcaption");
+    workImage.src = project.imageUrl;
+    workImage.alt = project.title;
+    workTitle.innerText = project.title;
+    workCard.dataset.category = project.category.name;
+    workCard.className = "workCard";
+    workCard.append(workImage, workTitle);
+    gallery.appendChild(workCard);
 }
 
 // ********** FILTER ***********//
 
-//get list of categories in array as unique objects
+// Fonction pour obtenir les catégories uniques
 function getCategories(worksData) {
     const listOfCategories = new Set();
-    //get set of string categories
-    worksData.forEach((work) => {
-      listOfCategories.add(JSON.stringify(work.category));
+    worksData.forEach(work => {
+        listOfCategories.add(JSON.stringify(work.category));
     });
-    //push stringified categories in array
     const arrayOfStrings = [...listOfCategories];
-    //parse array to get objects back
-    const categories = arrayOfStrings.map((s) => JSON.parse(s));
-    
-    return categories;
-}  
+    return arrayOfStrings.map(s => JSON.parse(s));
+}
 
-//init filter buttons
+// Fonction pour initier les boutons de filtre
 function categoryFilter(categories, filter) {
     const button = document.createElement("button");
     button.innerText = "Tous";
@@ -68,10 +68,10 @@ function categoryFilter(categories, filter) {
     functionFilter();
 }
 
-//create filter buttons
+// Fonction pour créer les boutons de filtre
 function filterButtons(categories, filter) {
-    categories.forEach((categorie) => {
-      createButtonFilter(categorie, filter);
+    categories.forEach(categorie => {
+        createButtonFilter(categorie, filter);
     });
 }
 
@@ -83,132 +83,28 @@ function createButtonFilter(categorie, filter) {
     filter.appendChild(button);
 }
 
-// Gallery filter
+// Fonction pour gérer le filtre de la galerie
 function functionFilter() {
     const filterButtons = document.querySelectorAll(".filterButton");
-    //identify which filter button has been clicked
-    filterButtons.forEach((i) => {
-      i.addEventListener("click", function () {
-        toggleProjects(i.dataset.category);
-      });
+    filterButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            toggleProjects(button.dataset.category);
+        });
     });
 }
 
-//if button "tous" active, display all projects, else display only those with same dataset category
+// Fonction pour afficher les projets selon le filtre sélectionné
 function toggleProjects(datasetCategory) {
     const figures = document.querySelectorAll(".workCard");
-    if ("Tous" === datasetCategory) {
-      figures.forEach((figure) => {
-        figure.style.display = "block";
-      });
+    if (datasetCategory === "Tous") {
+        figures.forEach(figure => {
+            figure.style.display = "block";
+        });
     } else {
-      figures.forEach((figure) => {
-        figure.dataset.category === datasetCategory
-          ? (figure.style.display = "block")
-          : (figure.style.display = "none");
-      });
+        figures.forEach(figure => {
+            figure.dataset.category === datasetCategory
+                ? (figure.style.display = "block")
+                : (figure.style.display = "none");
+        });
     }
-}
-
-//*******MODAL*******//
-
-let modal = null; 
-
-// Ouvre la modale
-const openModal = function (e) {
-    e.preventDefault();
-    const target = document.getElementById('modal1');
-    target.style.display = null;
-    target.removeAttribute('aria-hidden');
-    target.setAttribute('aria-modal', 'true');
-    modal = target;
-    modal.addEventListener('click', closeModal);
-    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
-    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
-}
-
-// Permet de fermer la modal
-const closeModal = function (e) {
-    if (modal === null) return;
-    e.preventDefault();
-    modal.style.display = "none";
-    modal.setAttribute('aria-hidden', 'true');
-    modal.removeAttribute('aria-modal');
-    modal.removeEventListener('click', closeModal);
-    modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
-    modal.querySelector('.js-modal-stop').removeEventListener('click', stopPropagation);
-    modal = null;
-}
-
-const stopPropagation = function (e) {
-    e.stopPropagation();
-}
-
-document.querySelectorAll('.js-modal').forEach(a => {
-    a.addEventListener('click', openModal);
-})
-
-// Ferme la modal avec la touche echap
-window.addEventListener('keydown', function (e) {
-    if (e.key === "Escape" || e.key === "Esc") {
-        closeModal(e);
-    }
-})
-
-// Affiche la galerie de la modal
-function displayModalGallery(data) {
-    const titleModal = document.getElementById("titlemodal");
-    const modalGallery = document.querySelector(".modalGallery");
-    modalGallery.innerHTML = "";
-    titleModal.innerHTML = "Galerie photos";
-    //intégre tous les travaux dans un tableau
-    data.forEach((i) => {
-      //crée les tags
-      const workGalleryCard = document.createElement("figure");
-      const workImage = document.createElement("img");
-      const trashCan = document.createElement("i");
-      trashCan.classList.add("fa-solid", "fa-trash-can");
-
-      workImage.src = i.imageUrl;
-      workImage.alt = i.title;
-      workGalleryCard.dataset.category = i.category.name;
-      workGalleryCard.dataset.id = i.id;  // Ajout de l'ID du travail
-      console.log("ID défini pour le travail:", i.id);
-      workGalleryCard.className = "workGalleryCard";
-      //references to DOM
-      workGalleryCard.append(workImage, trashCan);
-      modalGallery.appendChild(workGalleryCard);
-    });
-
-    //delete a project
-    const deleteWork = document.querySelectorAll(".fa-trash-can");
-    deleteWork.forEach((icon) => {
-      icon.addEventListener("click", function() {
-        const workGalleryCard = icon.closest(".workGalleryCard");
-        const workId = workGalleryCard.dataset.id;
-        deleteProject(workId, workGalleryCard);
-      });
-    });
-}
-
-function deleteProject(workId, workGalleryCard) {
-    const authToken = localStorage.getItem('authToken'); // Récupérer le token depuis le local storage
-  
-    fetch(`${catchAPIurl}/works/${workId}`, {
-      method: "DELETE",
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${authToken}` // Ajoutez le token d'authentification
-      }
-    })
-      .then((response) => {
-        if (response.ok) {
-          // supprime l'élément du DOM
-          workGalleryCard.remove(); 
-          console.log(`Votre projet ${workId} a été supprimé avec succès`);
-        } else {
-          console.error(`Erreur lors de la suppression`); 
-        }
-      })
-      .catch(error => console.error('Error:', error));
 }
